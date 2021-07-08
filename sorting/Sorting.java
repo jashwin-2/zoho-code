9d970ea6adda159b010310656b6827aa04789d69
jashwin-2 <74712538+jashwin-2@users.noreply.github.com>
2021-07-08 04:48:42 -0700

    Add files via upload

---
+++ sorting/Sorting.java
@@ -0,0 +1,92 @@
+package sorting;
+
+public class Sorting {
+
+	    public void sort(int arr[])
+	    {
+	        int n = arr.length;
+	 
+	        for (int i = n / 2 - 1; i >= 0; i--)
+	            heapify(arr, n, i);
+	 
+	        for (int i = n - 1; i > 0; i--) {
+	            int temp = arr[0];
+	            arr[0] = arr[i];
+	            arr[i] = temp;
+	 
+	            heapify(arr, i, 0);
+	        }
+	    }
+	 
+	    void heapify(int arr[], int n, int i)
+	    {
+	        int largest = i; 
+	        int l = 2 * i + 1; 
+	        int r = 2 * i + 2; 
+	 
+	        if (l < n && arr[l] > arr[largest])
+	            largest = l;
+	 
+	        if (r < n && arr[r] > arr[largest])
+	            largest = r;
+	 
+	        if (largest != i) {
+	            int swap = arr[i];
+	            arr[i] = arr[largest];
+	            arr[largest] = swap;
+	 
+	            heapify(arr, n, largest);
+	        }
+	    }
+	  
+	    
+	  
+
+		void bubbleSort(int arr[])
+	    {
+	        int n = arr.length;
+	        for (int i = 0; i < n-1; i++)
+	            for (int j = 0; j < n-i-1; j++)
+	                if (arr[j] > arr[j+1])
+	                {
+	                   
+	                    int temp = arr[j];
+	                    arr[j] = arr[j+1];
+	                    arr[j+1] = temp;
+	                }
+	    }
+	 
+	    static void printArray(int arr1[])
+	    {
+	        int n = arr1.length;
+	        for (int i = 0; i < n; ++i)
+	            System.out.print(arr1[i] + " ");
+	        System.out.println();
+	    }
+	 
+	    public static void main(String args[])
+	    {
+	        int arr[] = { 12, 11, 13, 5, 6, 7 };
+	        int arr1[] = { 12, 11, 13, 5, 6, 7 };
+	 
+	       Sorting ob = new Sorting();
+	        ob.sort(arr);
+	 
+	        System.out.println("Sorted array using heap sort");
+	        printArray(arr);
+	        
+	        ob.bubbleSort(arr1);
+	        System.out.println("Sorted array using bubble sort");
+	        printArray(arr1);
+	        QuickSort q=new QuickSort();
+	       
+	        int[] arr2 = { 10, 7, 8, 9, 1, 5 };
+			int n = arr.length;
+			q.quickSort(arr2, 0, n - 1);
+			System.out.println("Sorted array using quick sort");
+			q.printArray(arr2, n);
+	        
+	        
+	    }
+	
+}
